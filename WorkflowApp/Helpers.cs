4302ff5a72f55c7e9de57b9331411d329b911f18
Daniel Judge <dwjref@gmail.com>
2015-05-12 10:11:43 -0500

    removed not needed GetTitleOfDataScoringDateFilter
    it's already in the ScoringFilter class

--- WorkflowApp/Helpers.cs
+++ WorkflowApp/Helpers.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Windows.Forms;
-using ActiLifeAPILibrary.Models.Actions;
 using Ookii.Dialogs;
 
 namespace WorkflowApp
@@ -74,38 +72,6 @@ namespace WorkflowApp
                 }
             }
             return string.Empty;
-        }
-
-        public static string GetTitleOfDataScoringDateFilter(this ScoringFilter scoringFilter)
-        {
-            var dayNames = CultureInfo.CurrentCulture.DateTimeFormat.DayNames;
-            string title = "";
-            switch (scoringFilter.Type)
-            {
-                case ScoringFilter.FilterType.AllDays:
-                    title = "All Days";
-                    break;
-                case ScoringFilter.FilterType.Weekdays:
-                    title = "Weekdays";
-                    break;
-                case ScoringFilter.FilterType.Weekends:
-                    title = "Weekends";
-                    break;
-                case ScoringFilter.FilterType.DayOfTheWeek:
-                    title = dayNames[scoringFilter.DayOfTheWeek];
-                    break;
-                case ScoringFilter.FilterType.SpecificDate:
-                    title = scoringFilter.SpecificDate.ToShortDateString();
-                    break;
-                case ScoringFilter.FilterType.DayRange:
-                    title = string.Format("{0} -\r\n{1}", dayNames[scoringFilter.DayRangeStart], dayNames[scoringFilter.DayRangeEnd]);
-                    break;
-                case ScoringFilter.FilterType.DateRange:
-                    title = string.Format("{0} -\r\n{1}", scoringFilter.DateRangeStart.ToShortDateString(), scoringFilter.DateRangeEnd.ToShortDateString());
-                    break;
-            }
-
-            return title;
         }
     }
 }
